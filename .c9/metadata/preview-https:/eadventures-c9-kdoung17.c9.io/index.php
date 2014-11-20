fb55f317c5bd675846dafb790fcc7c363710233d
kdoung17 <kdoung17@gmail.com>
2014-11-20 22:41:37 +0000

    working site

--- .c9/metadata/preview-https:/eadventures-c9-kdoung17.c9.io/index.php
+++
@@ -1,1 +0,0 @@
-{"filter":false,"title":"[P] https://eadventures-c9-kdoung17.c9.io/index.php","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://eadventures-c9-kdoung17.c9.io/index.php","previewer":"preview.browser"},"timestamp":1411777900398,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
---
+++ pages/home.php
@@ -0,0 +1,36 @@
+<div class='home'>
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Explore Yosemite Backcountry</h1>
+        <p>Experience Yosemite in a whole new way</p>
+        <a href="#">Learn More</a>
+      </div>
+    </div> 
+    
+    <div class="featured-trips">
+        <div class="container">
+            <h2>Featured Trips</h2>
+            <div class='row'>
+            
+            <?php
+            $result = mysqli_query($con,"SELECT * FROM trips;");
+            while($row = mysqli_fetch_array($result)) {
+            ?>
+	            <div class='col-xs-12 col-sm-12 col-md-4'>
+	                <div class="thumbnail">
+                        <img src=<?php echo $row["image"]; ?> >
+                        <div class="caption">
+                        <h3><?php echo $row["trip_name"]; ?></h3>
+                         <p><?php echo $row["description"]; ?></p>
+                        <p><a href="booking.php" class="btn btn-primary" role="button">Book now!</a></p>
+                      </div>
+                    </div>
+	            </div>
+	            
+	        <?php  }
+	        mysqli_close($con);
+            ?>
+                
+	        </div>
+        </div> 
+    </div> 
